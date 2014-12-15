95f0fc2e5b8ca25e7a57744bda055c5fa64b736c
john ko <johnsko89@gmail.com>
2014-12-15 16:47:00 -0500

    created the show page with controllers and view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,21 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    render plain: params[:question].inspect
+    @question = Question.new(question_params)
+
+    @question.save
+    redirect_to @question
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  
+  private
+    def question_params
+      params.require(:question).permit(:title, :text)
+    end
+
+
 end
