01c05053052a636fa95d5248601b2fadfa0bc24d
john ko <johnsko89@gmail.com>
2014-12-16 16:06:29 -0500

    created an edit/delete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,6 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-
   def create
     @question = Question.new(question_params)
 
@@ -22,11 +21,29 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
 
   private
     def question_params
       params.require(:question).permit(:title, :text)
     end
-
 
 end
