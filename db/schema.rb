322275044ed9201d56161e459a22e4b646dbfc46
john ko <johnsko89@gmail.com>
2014-12-16 16:46:30 -0500

    created an answer models with databse

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215212525) do
+ActiveRecord::Schema.define(version: 20141216214255) do
+
+  create_table "answers", force: true do |t|
+    t.string   "user"
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "questions", force: true do |t|
     t.string   "title"
